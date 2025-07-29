232ce13132c350aac39d9f6e47e5c8a91b62c453
Alina Buzachis <abuzachis@redhat.com>
2025-07-29 16:02:04 +0200

    Isort
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/devopsguru_resource_collection.py
+++ plugins/modules/devopsguru_resource_collection.py
@@ -169,12 +169,14 @@ except ImportError:
     pass  # Handled by AnsibleAWSModule
 
 
-from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
-from ansible_collections.amazon.aws.plugins.module_utils.exceptions import (
-    AnsibleAWSError,
-)
-from ansible_collections.amazon.aws.plugins.module_utils.modules import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
+from ansible.module_utils.common.dict_transformations import \
+    camel_dict_to_snake_dict
+from ansible_collections.amazon.aws.plugins.module_utils.exceptions import \
+    AnsibleAWSError
+from ansible_collections.amazon.aws.plugins.module_utils.modules import \
+    AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.retries import \
+    AWSRetry
 
 
 @AWSRetry.jittered_backoff(retries=10)
